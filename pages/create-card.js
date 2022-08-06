0d03a6196c26eddc29f82cdf051e39d2620569a9
Hannah Steil <h-steil@t-online.de>
2022-08-06 10:56:39 +0200

    Add saving in database

--- pages/create-card.js
+++ pages/create-card.js
@@ -1,21 +1,44 @@
 import { useRouter } from "next/router";
+import { useFetch } from "../src/hooks/useFetch";
 
 export default function CreatCard() {
   const router = useRouter();
+  const fetchApi = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const content = event.target.inputContent.value;
+    const author = event.target.inputAuthor.value;
+
+    console.log(JSON.stringify({ content, author }));
+
+    await fetchApi("/api/card/create", {
+      method: "POST",
+      body: JSON.stringify({ content, author }),
+    });
+  }
 
   return (
     <>
       <h1>Creat new Card</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="inputContent"></label>
         <input
           type="text"
           id="inputContent"
+          name="inputContent"
           placeholder="I want to talk about ..."
           required
         />
         <label htmlFor="inputAuthor"></label>
-        <input type="text" id="inputAuthor" placeholder="Jane Doe" required />
+        <input
+          type="text"
+          id="inputAuthor"
+          name="inputAuthor"
+          placeholder="Jane Doe"
+          required
+        />
         <button>Submit</button>
         <button type="button" onClick={() => router.push("/")}>
           Cancel

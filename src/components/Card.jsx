e57be8ad3ee1b31c8577ac1d98f9f94b0a0254a7
Hannah Steil <h-steil@t-online.de>
2022-08-10 15:26:48 +0200

    Add minimal styling to card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card({ content, author }) {
   return (
-    <div>
+    <div style={{ border: "1px solid black", width: "300px" }}>
       <p>{content}</p>
       <p>{author}</p>
     </div>
